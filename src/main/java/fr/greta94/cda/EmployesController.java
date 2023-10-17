48b6d599cd83f5c914d19ca63a192b079adbb54e
martin <martin;guy@netcourrier.com>
2023-10-17 14:35:28 +0200

    2ème commit

--- src/main/java/fr/greta94/cda/EmployesController.java
+++ src/main/java/fr/greta94/cda/EmployesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+// controleur pour apprendre la sécurité
 @RestController
 //@RequestMapping("/api")
 public class EmployesController {
